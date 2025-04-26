800076578e20eee3e4d70a6f2a0a97e935770e85
liuqiao <349953@whut.edu.cn>
2025-04-26 23:22:37 +0800

    1 增加了阻塞获取锁的方式 2 修改一下注释

--- src/main/java/top/liuqiao/thumb/util/lock/DistributedLockUtil.java
+++ src/main/java/top/liuqiao/thumb/util/lock/DistributedLockUtil.java
@@ -22,14 +22,15 @@ public class DistributedLockUtil {
 
     /**
      * 尝试获取锁, 获取锁成功后会执行相关代码
-     * @param lockName 锁的全名
-     * @param waitTime 获取锁最长等待时间
-     * @param releaseTime 锁最长占用时间
-     * @param unit 时间单位
-     * @param supplier 加锁执行什么逻辑
+     *
+     * @param lockName         锁的全名
+     * @param waitTime         获取锁最长等待时间
+     * @param releaseTime      锁最长占用时间
+     * @param unit             时间单位
+     * @param supplier         加锁执行什么逻辑
      * @param exceptionHandler 执行过程中出现异常处理器
+     * @param <T>              返回值类型
      * @return 加锁执行逻辑的返回值 <br> NULL 为加锁失败或者 {@code supplier} 返回 NULL
-     * @param <T> 返回值类型
      */
     public <T> T tryLock(String lockName,
                          long waitTime, long releaseTime, TimeUnit unit,
@@ -55,9 +56,33 @@ public class DistributedLockUtil {
         return null;
     }
 
+    /**
+     * 尝试获取锁, 锁的等待时间和持续时间均采用默认模式
+     */
     public <T> T tryLock(String lockName, Supplier<T> supplier) {
         return tryLock(lockName,
                 -1L, -1L, TimeUnit.MILLISECONDS,
                 supplier, LockExceptionHandler.DEFAULT_HANDLER);
+    }
+
+    /**
+     * 一定获取到锁, 不然一直等待
+     */
+    public <T> T lock(String lockName,
+                      long releaseTime, TimeUnit unit,
+                      Supplier<T> supplier,
+                      LockExceptionHandler exceptionHandler) {
+        RLock lock = redissonClient.getLock(lockName);
+
+        lock.lock(releaseTime, unit);
+
+        try {
+            return supplier.get();
+        } catch (Throwable throwable) {
+            exceptionHandler.handler(throwable);
+        } finally {
+            lock.unlock();
+        }
+        return null;
     }
 }
